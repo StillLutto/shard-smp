1465f8ff111669abd6e9846b9fcefc79504f91e1
StillLutto <iameinsteinhe@gmail.com>
2024-04-14 01:45:55 +0200

    Initial commit

---
+++ src/main/java/me/lutto/shardsmp/ShardSMP.kt
@@ -0,0 +1,11 @@
+package me.lutto.shardsmp
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ShardSMP : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+}
