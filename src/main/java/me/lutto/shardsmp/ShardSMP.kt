cdd4755d8983ba72dd7f66d6850a74a6b81963c4
StillLutto <iameinsteinhe@gmail.com>
2024-05-05 15:28:46 +0200

    feat(tank-shield): add inventory potion effect

--- src/main/java/me/lutto/shardsmp/ShardSMP.kt
+++ src/main/java/me/lutto/shardsmp/ShardSMP.kt
@@ -30,6 +30,7 @@ class ShardSMP : JavaPlugin() {
         itemEffectManager.addItemEffect("lifestealer", PotionEffect(PotionEffectType.HEALTH_BOOST, PotionEffect.INFINITE_DURATION, 1), true)
         itemEffectManager.addItemEffect("poseidon_trident", PotionEffect(PotionEffectType.WATER_BREATHING, PotionEffect.INFINITE_DURATION, 0), true)
         itemEffectManager.addItemEffect("tank_shield", PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, PotionEffect.INFINITE_DURATION, 0), true)
+        itemEffectManager.addItemEffect("tank_shield", PotionEffect(PotionEffectType.SPEED, PotionEffect.INFINITE_DURATION, 0), true)
         itemEffectManager.addItemEffect("vanish_blade", PotionEffect(PotionEffectType.INVISIBILITY, PotionEffect.INFINITE_DURATION, 0), true)
 
         Bukkit.getPluginManager().registerEvents(DragonEggListener(this), this)
