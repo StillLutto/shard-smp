dca87d35e0d189a60262bf0b6605f3628cf79112
StillLutto <iameinsteinhe@gmail.com>
2024-06-22 13:18:03 +0200

    feat(poseidon-trident): make poseidon trident better

--- src/main/java/me/lutto/shardsmp/items/weapons/PoseidonTrident.kt
+++ src/main/java/me/lutto/shardsmp/items/weapons/PoseidonTrident.kt
@@ -16,6 +16,7 @@ import org.bukkit.inventory.RecipeChoice.ExactChoice
 import org.bukkit.inventory.ShapedRecipe
 import org.bukkit.inventory.meta.EnchantmentStorageMeta
 import org.bukkit.inventory.meta.ItemMeta
+import org.bukkit.scheduler.BukkitRunnable
 
 class PoseidonTrident(private val shardSMP: ShardSMP) : CustomCooldownItem(
     "poseidon_trident",
@@ -63,28 +64,36 @@ class PoseidonTrident(private val shardSMP: ShardSMP) : CustomCooldownItem(
         if (event.getItem() != shardSMP.itemManager.getItem("poseidon_trident")) return
 
         val world: World = event.getPlayer().world
-        val playerLocation: Location = event.getPlayer().location
         val radius = 10
-        for (x in -radius..radius) {
-            for (y in -radius..radius) {
-                for (z in -radius..radius) {
-                    val currentLocation = playerLocation.clone().add(x.toDouble(), y.toDouble(), z.toDouble())
 
-                    val currentBlock = world.getBlockAt(currentLocation)
-                    val blockType = currentBlock.type
-                    val blockData = currentBlock.blockData
+        object : BukkitRunnable() {
+            var count: Int = 1
 
-                    if (playerLocation.distance(currentLocation) > radius) continue
+            override fun run() {
+                val playerLocation: Location = event.getPlayer().location
 
-                    if (blockType == Material.WATER || blockType == Material.KELP || blockType == Material.KELP_PLANT || blockType == Material.SEAGRASS || blockType == Material.TALL_SEAGRASS) {
-                        currentBlock.type = Material.AIR
-                    } else if (blockData is Waterlogged && blockData.isWaterlogged) {
-                        blockData.isWaterlogged = false
-                        currentBlock.blockData = blockData
+                for (x in -radius..radius) {
+                    for (y in -radius..radius) {
+                        for (z in -radius..radius) {
+                            val currentLocation = playerLocation.clone().add(x.toDouble(), y.toDouble(), z.toDouble())
+                            val currentBlock = world.getBlockAt(currentLocation)
+                            val blockType = currentBlock.type
+                            val blockData = currentBlock.blockData
+
+                            if (playerLocation.distance(currentLocation) > radius) continue
+
+                            if (blockType == Material.WATER || blockType == Material.KELP || blockType == Material.KELP_PLANT || blockType == Material.SEAGRASS || blockType == Material.TALL_SEAGRASS) {
+                                currentBlock.type = Material.AIR
+                            } else if (blockData is Waterlogged && blockData.isWaterlogged) {
+                                blockData.isWaterlogged = false
+                                currentBlock.blockData = blockData
+                            }
+                        }
                     }
                 }
+                if (count++ == 200) this.cancel()
             }
-        }
+        }.runTaskTimer(shardSMP, 1, 1)
     }
 
 }
